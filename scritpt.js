f5142594d294e49117cbc73ebc49e05ae4b56bfe
Pedro Arthur <pedroarthurdr@hotmail.com>
2022-06-27 11:57:05 -0300

    iniciando js

--- scritpt.js
+++ scritpt.js
@@ -1,3 +1,37 @@
-function enviar() {
+
+function inserirnome(){
+    seunome = prompt('Qual seu nome?');
+    const name =
+    {
+      name:seunome
+    }
+
+    let promisse = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants',name);
+    promisse.then(sucesso);
+
+    promisse.catch(erro);
+
+
+
+}
+
+function sucesso(){
+    console.log(seunome)
+}
+
+function erro(){
+    alert('nome ja cadastrado')
+}
+
+
+function  enviarmsg(){
     alert()
 }
+
+
+function buscarmsg(){
+
+}
+
+
+inserirnome()
